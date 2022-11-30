b9f5a71d47c97b3da502f91842e847cae410d4e5
Spyro1001 <raymondhedgecock@gmail.com>
2022-11-29 19:08:06 -0700

    dictionary service

---
+++ dictionary/src/main/java/com/example/dictionary/service/DictionaryService.java
@@ -0,0 +1,53 @@
+package com.example.dictionary.service;
+
+import com.example.dictionary.model.Entry;
+import com.example.dictionary.reference.DictionaryReference;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class DictionaryService {
+
+    private static final String INVALID_REFERENCE = "Invalid Reference";
+
+    public Entry getWord(String word) {
+
+        String definition = DictionaryReference.getDictionary()
+                                               .get(word);
+        Entry entry = new Entry(word, definition);
+
+        if (definition == null) {
+            entry.setDefinition(INVALID_REFERENCE);
+        }
+
+        return entry;
+    }
+
+    public List<Entry> getWordsStartingWith(String value) {
+
+        return DictionaryReference.getDictionary()
+                                  .entrySet()
+                                  .stream()
+                                  .filter(entry -> entry.getKey()
+                                                        .startsWith(value))
+                                  .sorted(Map.Entry.comparingByKey(Comparator.naturalOrder()))
+                                  .map(entry -> new Entry(entry.getKey(), entry.getValue()))
+                                  .collect(Collectors.toList());
+    }
+
+    public List<Entry> getWordsThatContain(String value) {
+
+        return DictionaryReference.getDictionary()
+                                  .entrySet()
+                                  .stream()
+                                  .filter(entry -> entry.getKey()
+                                                        .contains(value))
+                                  .sorted(Map.Entry.comparingByKey(Comparator.naturalOrder()))
+                                  .map(entry -> new Entry(entry.getKey(), entry.getValue()))
+                                  .collect(Collectors.toList());
+    }
+}
