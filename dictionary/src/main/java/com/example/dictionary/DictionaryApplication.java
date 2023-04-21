df8e637f7ab88f08250ec5975d985e24a1a5d11a
spyro1001 <raymondhedgecock@gmail.com>
2023-04-20 19:27:26 -0700

    adding DictionaryReference

--- dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
+++ dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
@@ -11,14 +11,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class DictionaryApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext cxt = SpringApplication.run(DictionaryApplication.class, args);
-        String[] beans = cxt.getBeanDefinitionNames();
-        AtomicInteger counter = new AtomicInteger(1);
-        Arrays.stream(beans)
-              .sorted()
-              .map(name -> counter.getAndIncrement() + ": " + name)
-              .forEach(System.out::println);
-
+        SpringApplication.run(DictionaryApplication.class, args);
     }
 
 }
