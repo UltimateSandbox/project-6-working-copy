d73349412d0d293dfda2941f434ca29e33fccf32
spyro1001 <raymond.hedgecock@gmail.com>
2023-11-21 19:30:32 -0700

    adding dictionary code

--- dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
+++ dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
@@ -1,13 +1,18 @@
 package com.example.dictionary;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DictionaryApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(DictionaryApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(DictionaryApplication.class, args);
+        log.info("hello spring");
     }
 
 }
