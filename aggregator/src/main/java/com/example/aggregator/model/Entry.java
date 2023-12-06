45e27ca78a43a0f68aa465f754aeefdca037ba08
spyro1001 <raymond.hedgecock@gmail.com>
2023-12-05 19:12:52 -0700

    rest client additions

---
+++ aggregator/src/main/java/com/example/aggregator/model/Entry.java
@@ -0,0 +1,71 @@
+package com.example.aggregator.model;
+
+import java.util.Objects;
+
+public class Entry implements Comparable<Entry> {
+
+    private String word;
+    private String definition;
+
+    public Entry() {
+    }
+
+    public Entry(String word, String definition) {
+        this.word = word;
+        this.definition = definition;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entry entry = (Entry) o;
+        return Objects.equals(word, entry.word) && Objects.equals(definition, entry.definition);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word, definition);
+    }
+
+    @Override
+    public int compareTo(Entry that) {
+
+        if (this.word.compareTo(that.word) > 0) {
+            return 1;
+        } else if (this.word.compareTo(that.word) < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Entry{");
+        sb.append("word='")
+                .append(word)
+                .append('\'');
+        sb.append(", definition='")
+                .append(definition)
+                .append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
